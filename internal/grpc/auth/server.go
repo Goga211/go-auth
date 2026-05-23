0f75e8bf9b1aaa0a7035b112a80cf81cee36a0c9
Goga211 <antipingv2003@gmail.com>
2026-05-24 01:29:06 +0300

    handlers

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,22 +5,82 @@ import (
 
 	auth "github.com/Goga211/proto/gen/go/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+	) (token string, err error)
+
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+}
 
 type serverAPI struct {
 	auth.UnimplementedAuthServer
+	authentication Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	auth.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, authentication Auth) {
+	auth.RegisterAuthServer(gRPC, &serverAPI{authentication: authentication})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
+	if err := ValidateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.authentication.Login(ctx, req.GetEmail(), req.GetPassword())
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
 	return &auth.LoginResponse{
-		Token: "tokenExample",
+		Token: token,
 	}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
-	panic("implement me")
+	if err := ValidateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.authentication.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &auth.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func ValidateLogin(req *auth.LoginRequest) error {
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password required")
+	}
+
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+	return nil
+}
+
+func ValidateRegister(req *auth.RegisterRequest) error {
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password required")
+	}
+
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+	return nil
 }
