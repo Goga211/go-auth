46d436a451375b6094fd6c7d4a919ea0813f6aad
Goga211 <antipingv2003@gmail.com>
2026-05-25 01:20:02 +0300

    service

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,102 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/Goga211/go-auth/internal/domain/model"
+	"github.com/Goga211/go-auth/internal/storage"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (model.User, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (model.App, error)
+}
+
+func New(log *slog.Logger, userSaver UserSaver, appProvider AppProvider, userProvider UserProvider) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		appProvider: appProvider,
+		tokenTTL:    time.Hour,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email string, password string, appID int) (string, error) {
+	const operation = "auth.login"
+	log := a.log.With(
+		slog.String("operation", operation),
+		slog.String("email", email))
+
+	log.Info("login in progress")
+
+	user, err := a.usrProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found")
+			return "", fmt.Errorf("%s: %w", operation, ErrInvalidCredentials)
+		}
+		a.log.Warn("user not found")
+		return "", fmt.Errorf("%s: %w", operation, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Warn("invalid password")
+		return "", fmt.Errorf("%s: %w", operation, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", operation, err)
+	}
+
+	log.Info("user logged in")
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (int64, error) {
+	const operation = "auth.RegisterNewUser"
+
+	log := a.log.With(
+		slog.String("operation", operation),
+		slog.String("email", email))
+
+	log.Info("Registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate hash password", err)
+		return 0, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	uid, err := a.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user", err)
+		return 0, fmt.Errorf("%s: %w", operation, err)
+	}
+	log.Info("User registered successfully")
+	return uid, nil
+}
