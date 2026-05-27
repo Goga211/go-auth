0cb4d1e72ac3091aad0241c77791d92ce6231ced
Goga211 <antipingv2003@gmail.com>
2026-05-28 01:17:31 +0300

    migrations base

---
+++ cmd/migrator/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storagePath, "storage-path", "", "Path to a directory containing the migration files")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "Path to a directory containing the migration files")
+	flag.StringVar(&migrationsTable, "migrations-table", "", "Path to a table containing the migration files")
+	flag.Parse()
+
+	validatePaths(storagePath, migrationsPath)
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("No migrations found")
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("Migrations applied successfully")
+}
+
+func validatePaths(storagePath string, migrationsPath string) {
+	if storagePath == "" {
+		panic("storage-path flag is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path flag is required")
+	}
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,7 +3,5 @@ package storage
 import "errors"
 
 var (
-	ErrUserExist    = errors.New("user exist")
 	ErrUserNotFound = errors.New("user not found")
-	ErrAppNotFound  = errors.New("app not found")
 )
